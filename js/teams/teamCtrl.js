cead77874b7083d83848642345ec024c36ba680f
Dustin Myers <dustint314@gmail.com>
2015-04-20 12:22:31 -0600

    done with step 5

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -2,4 +2,13 @@ var app = angular.module('nbaRoutes');
 
 app.controller('teamCtrl', function($scope, $routeParams, teamService){
 
+	$scope.teamData = teamData();
+
+	$scope.newGame;
+
+	$scope.showNewGameForm = false;
+
+	$scope.toggleNewGameForm = function($scope.showNewGameForm) {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
 });
