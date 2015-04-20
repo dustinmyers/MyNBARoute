3c00d2027b4948cc43f7e0d14fc97639ddd9d08f
Dustin Myers <dustint314@gmail.com>
2015-04-20 11:57:04 -0600

    Done with step 3

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,38 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame(gameObj) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		parseInt(homeTeamScore);
+		parseInt(opponentScore);
+		if (gameObj.homeTeamScore > gameObj.opponentScore) {
+			gameObj.gameWon === true;
+		} else {
+			gameObj.gameWon === false;
+		}
+		$http.post(url)
+	};
+
+	this.getTeamData(team) {
+		var dfr = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http.get(url)
+			.then(fucntion(data) {
+				var results = data.data.results;
+				var wins = 0;
+				var losses = 0;
+				for (key in results) {
+					if (results.won === true);
+					wins++;
+				} else {
+					losses++;
+				}
+			results.wins = wins;
+			results.losses = losses;
+			console.log(results);
+			dfr.resolve(results)
+			})
+		return(defer.promise);	
+	}
+
 });
